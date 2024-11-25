9cfaa10cc15a24e7878ccccae97ed45d7077528c
Na9engS <109756371+Na9engS@users.noreply.github.com>
2024-11-26 03:04:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example: Simple alert for "Buy Now" button
+const buyButtons = document.querySelectorAll(".buy-now");
+
+buyButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        alert("This feature is under development!");
+    });
+});
